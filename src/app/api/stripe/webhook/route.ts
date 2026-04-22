05ab00f93c059f37fcd37a1c653828d491c6771b
Adrian Dabek <apd013@yahoo.com>
2026-04-21 23:02:12 -0700

    Fix Stripe CheckoutSession type

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: Request) {
   try {
     switch (event.type) {
       case 'checkout.session.completed': {
-        const session = event.data.object as Stripe.CheckoutSession
+        const session = event.data.object as Stripe.Checkout.Session
         const userId = session.metadata?.userId
         const mode = session.metadata?.mode
 
